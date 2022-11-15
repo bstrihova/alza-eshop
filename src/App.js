003ef37e16d86930e31889f2975e5b81b3f23398
barbora <47380995+bstrihova@users.noreply.github.com>
2022-11-15 22:25:25 +0100

    abstract fetching data from App

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import body from './body.json';
-import hardcodedData from './data.json';
+import React from 'react';
 import './index.scss';
-import Loader from './components/loader';
 import Header from './containers/Header';
 import Main from './containers/Main';
 
 function App() {
-    const [data, setData] = useState(null);
-    const [errors, setErrors] = useState(null);
-    const url = 'https://www.alza.cz/Services/RestService.svc/v2/products';
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(url, {
-                method: 'post',
-                body: JSON.stringify(body),
-                // I
-                mode: 'no-cors',
-            });
-            const response_data = response.json();
-
-            // if failed to load data, fallback on the hardcoded ones (had problems with CORS)
-            if (response.status === 200) {
-                setData(response_data.data);
-            } else {
-                setData(hardcodedData);
-            }
-
-            if (response_data.errors) {
-                setErrors(response_data.errors);
-            }
-        };
-
-        fetchData();
-    }, []);
-
     return (
         <div className="app">
             <Header />
-            {errors}
-            {data ? <Main data={data} /> : <Loader />}
+            <Main />
         </div>
     );
 }
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SortBtns from '../components/sortBtns';
 import Swiper from '../components/swiper';
 import Item from '../components/item/item';
 import ItemsWrapper from '../components/item/items-wrapper';
 import styled from 'styled-components';
 import { FlexDiv } from '../components/commonWrappers';
+import Loader from '../components/loader';
+import hardcodedData from '../data/data.json';
+import body from '../data/body.json';
 
 const StyledMain = styled.main`
     display: flex;
@@ -16,9 +19,41 @@ const StyledMain = styled.main`
     }
 `;
 
-function Main({ data }) {
+function Main() {
+    const [data, setData] = useState(null);
+    const [errors, setErrors] = useState(null);
+    const url = 'https://www.alza.cz/Services/RestService.svc/v2/products';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url, {
+                method: 'post',
+                body: JSON.stringify(body),
+                // I
+                mode: 'no-cors',
+            });
+            const response_data = response.json();
+
+            // if failed to load data, fallback on the hardcoded ones (had problems with CORS)
+            if (response.status === 200) {
+                setData(response_data.data);
+            } else {
+                setData(hardcodedData);
+            }
+
+            if (response_data.errors) {
+                setErrors(response_data.errors);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (!data) return <Loader />;
+
     return (
         <StyledMain>
+            {errors}
             <FlexDiv column gap={'8px'}>
                 <h3>Nejprodavanejsi</h3>
                 <Swiper data={data} />
